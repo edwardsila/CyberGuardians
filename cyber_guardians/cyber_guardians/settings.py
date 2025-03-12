4528313ba974ea50ae17a0b8d97ae5cd56fa991e
edwardsila <edwardsila000@gmail.com>
2025-03-12 15:20:41 +0300

    handled user aunthentication

--- cyber_guardians/cyber_guardians/settings.py
+++ cyber_guardians/cyber_guardians/settings.py
@@ -126,3 +126,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'  # Redirect to home after login
+LOGOUT_REDIRECT_URL = 'home'
